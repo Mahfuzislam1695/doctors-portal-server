275f0d1d1ef11f9f018c843958b64fefa1cb5f3a
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2022-02-20 21:52:53 +0600

    service

--- index.js
+++ index.js
@@ -7,42 +7,26 @@ const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
-// const serviceAccount ={
-//     dev: {
-//       type: process.env.TYPE,
-//       project_id: process.env.PROJECT_ID,
-//       private_key_id: process.env.PRIVATE_KEY_ID,
-//       private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), 
-//       client_email: process.env.CLIENT_EMAIL,
-//       client_id: process.env.CLIENT_ID,
-//       auth_uri: process.env.AUTH_URI,
-//       token_uri: process.env.TOKEN_URI,
-//       auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-//       client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-//     },
-//   };
-// var serviceAccount = require("./service-account.json");
-
-// var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-       credential: admin.credential.cert({
-        type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT
-      })
-     });
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// admin.initializeApp({
+//        credential: admin.credential.cert({
+//         type: process.env.FIREBASE_TYPE,
+//         project_id: process.env.FIREBASE_PROJECT_ID,
+//         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY,
+//         client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//         client_id: process.env.FIREBASE_CLIENT_ID,
+//         auth_uri: process.env.FIREBASE_AUTH_URI,
+//         token_uri: process.env.FIREBASE_TOKEN_URI,
+//         auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+//         client_x509_cert_url: process.env.FIREBASE_CLIENT
+//       })
+//      });
 
 
 app.use(cors());
