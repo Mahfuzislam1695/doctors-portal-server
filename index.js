b23be2cf585c505b8e77af742fe120d430e6e42b
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2022-02-23 09:21:30 +0600

    correction2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
 
         app.get('/appointments', async (req, res) => {
             const email = req.query.email;
-            const date = req.query.date;
+            const date =new Date(req.query.date).toLocaleDateString();
             const query = { email: email, date: date };
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
